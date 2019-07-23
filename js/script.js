78719ea72cd9196ffca4240aaf2396af9e8c8a2a
eternith <peterbuk8@gmail.com>
2019-07-23 16:42:15 -0700

    added leaflet map + mapbox

--- js/script.js
+++ js/script.js
@@ -29,3 +29,17 @@ $('.carousel').flickity({
   pageDots: false,
   autoPlay: 1500
 });
+
+var myMap = L.map('map').setView([49.2655, -123.106], 14);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox.streets',
+  accessToken: 'pk.eyJ1IjoicGV0ZXJidWs4IiwiYSI6ImNqeWdnOHgzZDAwcXgzY3Q0Z2oxcDY4NjAifQ.mAZ119eFPABKncLt5MdxsQ'
+}).addTo(myMap);
+
+var marker = L.marker([49.265, -123.101]).addTo(myMap)
+  .bindPopup('<b>Cafe Neuvo</b><br>1251 Main Street')
+  .openPopup();
+
